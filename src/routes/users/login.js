613676fe97daccd8ff532cbbcc1cc0d8e4466b22
talfumier <henri.talfumier@gmail.com>
2024-06-05 20:40:28 +0200

    User routes finalized

--- src/routes/users/login.js
+++ src/routes/users/login.js
@@ -1,50 +1,54 @@
-// // @ts-check
-// import express from "express";
-// import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
-// import {routeHandler} from "../../middleware/routeHandler.js";
-// import {User} from "../../models/mysqlModels.js";
-// import {Unauthorized} from "../../models/validation/errors.js";
-// import config from "../../config/config.json" assert {type: "json"};
-// import {environment} from "../../config/environment.js";
+// @ts-check
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {routeHandler} from "../../middleware/routeHandler.js";
+import {User} from "../../models/mysqlModels.js";
+import {Unauthorized} from "../../models/validation/errors.js";
+import config from "../../config/config.json" assert {type: "json"};
+import {environment} from "../../config/environment.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post(
-//   "/",
-//   routeHandler(async (req, res) => {
-//     const user = await User.findOne({
-//       where: {email: req.body.email},
-//     });
-//     if (user) {
-//       const pwd_valid = await bcrypt.compare(req.body.pwd, user.pwd);
-//       if (pwd_valid) {
-//         const token = jwt.sign(
-//           {
-//             id: user.id,
-//             last_name: user.last_name,
-//             first_name: user.first_name,
-//             email: user.email,
-//             role: user.role,
-//           },
-//           environment.sha256,
-//           {
-//             expiresIn: config.token_expires_in,
-//           }
-//         );
-//         await user.update({last_connection: new Date()});
-//         user.pwd = undefined;
-//         return res
-//           .header("x-auth-token", token)
-//           .header("access-control-expose-headers", ["x-auth-token"])
-//           .send({
-//             status: "OK",
-//             message: `User with id:${user.id} successfully logged-in.`,
-//             data: user,
-//           });
-//       }
-//     }
-//     return res.send(new Unauthorized("Wrong username or password."));
-//   })
-// );
-// export default router;
+router.post(
+  "/",
+  routeHandler(async (req, res) => {
+    const user = await User.findOne({
+      where: {email: req.body.email},
+    });
+    if (user) {
+      if (!user.validated)
+        return res.send(
+          new Unauthorized("Your account is still pending validation.")
+        );
+      const pwd_valid = await bcrypt.compare(req.body.pwd, user.pwd);
+      if (pwd_valid) {
+        const token = jwt.sign(
+          {
+            id: user.id,
+            last_name: user.last_name,
+            first_name: user.first_name,
+            email: user.email,
+            role: user.role,
+          },
+          environment.sha256,
+          {
+            expiresIn: config.token_expires_in,
+          }
+        );
+        await user.update({last_connection: new Date()});
+        user.pwd = undefined;
+        return res
+          .header("x-auth-token", token)
+          .header("access-control-expose-headers", ["x-auth-token"])
+          .send({
+            status: "OK",
+            message: `User with id:${user.id} successfully logged-in.`,
+            data: user,
+          });
+      }
+    }
+    return res.send(new Unauthorized("Wrong username or password."));
+  })
+);
+export default router;
--- src/routes/users/profile.js
+++ src/routes/users/profile.js
@@ -1,60 +1,60 @@
-// // @ts-check
-// import express from "express";
-// import bcrypt from "bcrypt";
-// import {authHandler} from "../../middleware/authHandler.js";
-// import {routeHandler} from "../../middleware/routeHandler.js";
-// import {User} from "../../models/mysqlModels.js";
-// import {validateUser} from "../../models/mysqlModels.js";
-// import {BadRequest, Unauthorized} from "../../models/validation/errors.js";
-// import {bodyCleanUp} from "../../models/validation/utilityFunctions.js";
-// import {environment} from "../../config/environment.js";
+// @ts-check
+import express from "express";
+import bcrypt from "bcrypt";
+import {authHandler} from "../../middleware/authHandler.js";
+import {routeHandler} from "../../middleware/routeHandler.js";
+import {User} from "../../models/mysqlModels.js";
+import {validateUser} from "../../models/mysqlModels.js";
+import {BadRequest, Unauthorized} from "../../models/validation/errors.js";
+import {bodyCleanUp} from "../../models/validation/utilityFunctions.js";
+import {environment} from "../../config/environment.js";
 
-// const router = express.Router();
-
-// router.patch(
-//   "/",
-//   [authHandler],
-//   routeHandler(async (req, res) => {
-//     const id = req.user.id;
-//     const user = await User.findByPk(id, {
-//       attributes: {exclude: ["pwd"]},
-//     });
-//     if (!user) return res.send(new BadRequest(`User with id:${id} not found.`));
-//     req.body = bodyCleanUp(req.body);
-//     const error = validateUser(req.body, "patch").error;
-//     if (error) return res.send(new BadRequest(error.details[0].message));
-//     if (req.body.role && req.body.role === "admin")
-//       return res.send(
-//         new Unauthorized("admin account cannot be modified through the API.")
-//       );
-//     if (req.body.pwd)
-//       req.body.pwd = await bcrypt.hash(req.body.pwd, environment.salt_rounds);
-//     await user.update(req.body);
-//     res.send({
-//       status: "OK",
-//       message: `User with id:${id} successfully updated.`,
-//       data: user,
-//     });
-//   })
-// );
-// router.delete(
-//   "/",
-//   [authHandler],
-//   routeHandler(async (req, res) => {
-//     const id = req.user.id;
-//     const user = await User.findByPk(id);
-//     if (!user) return res.send(new BadRequest(`User with id:${id} not found.`));
-//     if (user.role === "admin")
-//       return res.send(
-//         new Unauthorized("admin account cannot be deleted through the API.")
-//       );
-//     await user.destroy(); //any related record in table users_salons will be removed by CASCADE DELETE
-//     user.pwd = undefined; //does not return the password
-//     res.send({
-//       status: "OK",
-//       message: `User with id:${id} successfully deleted.`,
-//       data: user,
-//     });
-//   })
-// );
-// export default router;
+const router = express.Router();
+//ONLY LOGGED IN USER CAN BE UPDATED OR DELETED
+router.patch(
+  "/",
+  [authHandler],
+  routeHandler(async (req, res) => {
+    const id = req.user.id; //user.id coming from authHandler
+    const user = await User.findByPk(id, {
+      attributes: {exclude: ["pwd"]},
+    });
+    if (!user) return res.send(new BadRequest(`User with id:${id} not found.`));
+    req.body = bodyCleanUp(req.body);
+    const error = validateUser(req.body, "patch").error;
+    if (error) return res.send(new BadRequest(error.details[0].message));
+    if (req.body.role)
+      return res.send(
+        new Unauthorized("account role cannot be modified through the API.")
+      );
+    if (req.body.pwd)
+      req.body.pwd = await bcrypt.hash(req.body.pwd, environment.salt_rounds);
+    await user.update(req.body);
+    res.send({
+      status: "OK",
+      message: `User with id:${id} successfully updated.`,
+      data: user,
+    });
+  })
+);
+router.delete(
+  "/",
+  [authHandler],
+  routeHandler(async (req, res) => {
+    const id = req.user.id; //user.id coming from authHandler
+    const user = await User.findByPk(id);
+    if (!user) return res.send(new BadRequest(`User with id:${id} not found.`));
+    if (user.role === "admin")
+      return res.send(
+        new Unauthorized("admin account cannot be deleted through the API.")
+      );
+    await user.destroy();
+    user.pwd = undefined; //does not return the password
+    res.send({
+      status: "OK",
+      message: `User with id:${id} successfully deleted.`,
+      data: user,
+    });
+  })
+);
+export default router;
